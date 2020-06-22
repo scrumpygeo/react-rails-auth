ee60211e7afd77269864af778356df9976e25de3
scrumpygeo <scrumpygeo@yahoo.co.uk>
2020-06-22 17:12:15 +0100

    check if user logged in

--- src/components/app.js
+++ src/components/app.js
@@ -1,5 +1,6 @@
 import React, { Component } from "react";
 import { BrowserRouter, Switch, Route } from "react-router-dom";
+import axios from "axios";
 import Home from "./Home";
 import Dashboard from "./Dashboard";
 
@@ -8,6 +9,37 @@ export default class App extends Component {
     loggedInStatus: "NOT_LOGGED_IN",
     user: {},
   };
+
+  checkLoginStatus = () => {
+    axios
+      .get("http://localhost:5000/logged_in", { withCredentials: true })
+      .then((response) => {
+        if (
+          response.data.logged_in &&
+          this.state.loggedInStatus === "NOT_LOGGED_IN"
+        ) {
+          this.setState({
+            loggedInStatus: "LOGGED_IN",
+            user: response.data.user,
+          });
+        } else if (
+          !response.data.logged_in &&
+          this.state.loggedInStatus === "LOGGED_IN"
+        ) {
+          this.setState({
+            loggedInStatus: "NOT_LOGGED_IN",
+            user: {},
+          });
+        }
+      })
+      .catch((error) => {
+        console.log("Check login error:", error);
+      });
+  };
+
+  componentDidMount() {
+    this.checkLoginStatus();
+  }
 
   handleLogin = (data) => {
     this.setState({
