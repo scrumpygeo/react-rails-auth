684bc39d6025abd216a4c32f1a47e1f0e8404c3a
scrumpygeo <scrumpygeo@yahoo.co.uk>
2020-06-22 09:15:35 +0100

    add route to home and dashboard

--- src/components/app.js
+++ src/components/app.js
@@ -1,11 +1,18 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
+import { BrowserRouter, Switch, Route } from "react-router-dom";
+import Home from "./Home";
+import Dashboard from "./Dashboard";
 
 export default class App extends Component {
   render() {
     return (
-      <div className='app'>
-        <h1>DevCamp React Starter</h1>
-        <h2>React Redux Router</h2>
+      <div className="app">
+        <BrowserRouter>
+          <Switch>
+            <Route exact path={"/"} component={Home} />
+            <Route exact path={"/dashboard"} component={Dashboard} />
+          </Switch>
+        </BrowserRouter>
       </div>
     );
   }
